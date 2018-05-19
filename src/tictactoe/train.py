d68c7012f9fb291c3f5dc0a1e6b1bc681b002fa8
Mason Kramer <mason.kramer@gmail.com>
2018-05-19 10:14:00 -0700

    training script

--- src/tictactoe/train.py
+++ src/tictactoe/train.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
 import glob
 
+def _parse_function(example_proto):
+  features = {"game": tf.FixedLenFeature((), tf.int64),
+              "choice": tf.FixedLenFeature((), tf.float32, default_value=0.0)}
+  parsed_features = tf.parse_single_example(example_proto, features)
+  return parsed_features["game"], parsed_features["choice"]
+
 num_epochs = 10
 dataset_dir = "src/tictactoe/gamedata"
 files = glob.glob("{}/*".format(dataset_dir))
@@ -11,34 +17,28 @@ dataset = dataset.shuffle(buffer_size=100000)
 dataset = dataset.batch(32)
 print("loaded data")
 
-graph_filename = src/tictactoe/simple_net.pb"
+graph_filename = "src/tictactoe/simple_net.pb"
 print("loading graph at '{}'".format(graph_filename))
-with gfile.FastGFile("src/tictcatoe/simple_net.pb",'rb') as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
-    sess.graph.as_default()
-    tf.import_graph_def(graph_def, name='')
+with tf.Session() as sess:
+    with tf.gfile.FastGFile(graph_filename,'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        sess.graph.as_default()
+        tf.import_graph_def(graph_def, name='')
 
-train = sess.graph.get_tensor_by_name('train')
-example_ph = sess.graph.get_tensor_by_name('x')
-label_ph = sess.graph.get_tensor_by_name('y_true')
-iterator = dataset.make_initializable_iterator()
-next_element = iterator.get_next()
+    train = sess.graph.get_operation_by_name('train')
+    example_ph = sess.graph.get_operation_by_name('x')
+    label_ph = sess.graph.get_operation_by_name('y_true')
+    iterator = dataset.make_initializable_iterator()
+    next_element = iterator.get_next()
 
-sess.run(iterator.intializer)
-while True:
-    for _ in range(num_epochs):
-        sess.run(iterator.initializer)
-        try:
-            example, label = sess.run(next_element)
+    sess.run(iterator.initializer)
+    while True:
+        for _ in range(num_epochs):
+            sess.run(iterator.initializer)
+            try:
+                example, label = sess.run(next_element)
 
-            sess.run(train)
-        except tf.errors.OutOfRangeError:
-            break
-
-
-def _parse_function(example_proto):
-  features = {"game": tf.FixedLenFeature((), tf.int64),
-              "choice": tf.FixedLenFeature((), tf.float32, default_value=0.0)}
-  parsed_features = tf.parse_single_example(example_proto, features)
-  return parsed_features["game"], parsed_features["choice"]
+                sess.run(train)
+            except tf.errors.OutOfRangeError:
+                break
